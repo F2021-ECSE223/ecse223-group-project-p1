e08ef461e548fbae4daf98e2b15335be9ac2ade3
Alexandre Chiasera <alexandre.chiasera@mail.mcgill.ca>
2021-10-16 18:25:02 -0400

    Added member methods for the controller and assigned steps definitions

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
@@ -1,14 +1,26 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 import java.sql.Date;
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.User;
 
 public class ClimbSafeFeatureSet1Controller {
+	
+	private static ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
 
   public static void setup(Date startDate, int nrWeeks, int priceOfGuidePerWeek)
       throws InvalidInputException {}
 
-  public static void deleteMember(String email) {}
-
+  public static void deleteMember(String email) {
+	 
+	 User user = User.getWithEmail(email);
+	 if(user != null) {
+		 user.delete();
+	 }
+	  
+  } 
+  
   public static void deleteGuide(String email) {}
 
   // this method needs to be implemented only by teams with seven team members
--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P1StepDefinitions.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P1StepDefinitions.java
@@ -4,7 +4,12 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.util.List;
+
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet4Controller;
 import ca.mcgill.ecse.climbsafe.controller.InvalidInputException;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
@@ -22,7 +27,7 @@ public class P1StepDefinitions {
     
     List<Object> columns = (dataTable.asLists(Object.class)).get(1);
 
-    assertEquals(((java.sql.Date) columns.get(0)), climbSafe.getStartDate());
+    assertEquals(((java.sql.Date)columns.get(0)), climbSafe.getStartDate());
     assertEquals(((int) columns.get(1)), climbSafe.getNrWeeks());
     assertEquals(((int) columns.get(2)), climbSafe.getPriceOfGuidePerWeek());
 
@@ -34,13 +39,14 @@ public class P1StepDefinitions {
   @Given("the following pieces of equipment exist in the system: \\(p1)")
   public void the_following_pieces_of_equipment_exist_in_the_system_p1(
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
+	  var rows = dataTable.asMaps();
+	  for (var row : rows) {
+	      String name = row.get("name");
+	      Integer weight =  Integer.parseInt(row.get("weight"));
+	      Integer pricePerWeek = Integer.parseInt(row.get("pricePerWeek"));
+	      climbSafe.addEquipment(name, weight, pricePerWeek);
+	    }
+	  
     throw new io.cucumber.java.PendingException();
   }
 
@@ -77,6 +83,7 @@ public class P1StepDefinitions {
 
   @Then("the number of pieces of equipment in the system shall be {string} \\(p1)")
   public void the_number_of_pieces_of_equipment_in_the_system_shall_be_p1(String string) {
+	  
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
@@ -84,13 +91,39 @@ public class P1StepDefinitions {
   @Then("the piece of equipment with name {string}, weight {string}, and price per week {string} shall not exist in the system \\(p1)")
   public void the_piece_of_equipment_with_name_weight_and_price_per_week_shall_not_exist_in_the_system_p1(
       String string, String string2, String string3) {
-    // Write code here that turns the phrase above into concrete actions
+	  
+	  String name = string;
+	  Integer weight = Integer.parseInt(string2);
+	  Integer pricePerWeek = Integer.parseInt(string3);
+	  
+	  for(Equipment pieceOfEquipment: climbSafe.getEquipment()) { //for each piece of equipment in the climbSafe system (for the admin)
+		  if(pieceOfEquipment.getName() == name && pieceOfEquipment.getWeight() == weight && pieceOfEquipment.getPricePerWeek() == pricePerWeek)
+		  {
+			  fail("The piece of equipment <" + pieceOfEquipment.getName() + "> has not been deleted from the system"); //if everything matches, it means the object is not deleted.
+		  }
+	  }
     throw new io.cucumber.java.PendingException();
   }
 
   @Then("the piece of equipment with name {string}, weight {string}, and price per week {string} shall exist in the system \\(p1)")
   public void the_piece_of_equipment_with_name_weight_and_price_per_week_shall_exist_in_the_system_p1(
       String string, String string2, String string3) {
+	  
+	  String name = string;
+	  Integer weight = Integer.parseInt(string2);
+	  Integer pricePerWeek = Integer.parseInt(string3);
+	  Boolean isEquipmentInTheSystem = false;
+	  
+	  for(Equipment pieceOfEquipment: climbSafe.getEquipment()) { //for each piece of equipment in the climbSafe system (for the admin)
+		  if(pieceOfEquipment.getName() == name && pieceOfEquipment.getWeight() == weight && pieceOfEquipment.getPricePerWeek() == pricePerWeek)
+		  {
+			   isEquipmentInTheSystem = true;//if everything matches --> object in the system.
+		  }
+	  }
+	  
+	  assertEquals(true, isEquipmentInTheSystem);
+	  assertEquals("", error);
+	  assertEquals(0, errorCount);	  
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
@@ -132,5 +165,16 @@ public class P1StepDefinitions {
     // For other transformations you can register a DataTableType.
     throw new io.cucumber.java.PendingException();
   }
+  
+  private void callController(Executable executable) {
+	    try {
+	      executable.execute();
+	    } catch (InvalidInputException e) {
+	      error += e.getMessage();
+	      errorCount += 1;
+	    } catch (Throwable t) {
+	      fail();
+	    }
+	  }
 
 }
