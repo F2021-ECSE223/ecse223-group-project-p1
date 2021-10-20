134afc42cc41e02bc826f7f5957bdc247d8b7293
aasmaa11 <90011767+aasmaa11@users.noreply.github.com>
2021-10-20 02:15:49 -0400

    Update ClimbSafeFeatureSet4Controller.java
    Update method updateEquipment()

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -1,11 +1,58 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.Equipment;
+
 public class ClimbSafeFeatureSet4Controller {
+
+  public static ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
 
   public static void addEquipment(String name, int weight, int pricePerWeek)
       throws InvalidInputException {}
 
   public static void updateEquipment(String oldName, String newName, int newWeight,
-      int newPricePerWeek) throws InvalidInputException {}
+      int newPricePerWeek) throws InvalidInputException {
+
+    var error = "";
+    if (newWeight <= 0) {
+      error += "The weight must be greater than zero";
+    }
+    if (newPricePerWeek < 0) {
+      error += "The price per week must be greater than or equal to zero";
+    }
+    if (newName.isEmpty()) {
+      error += "The name must not be empty";
+    }
+    if (!error.isEmpty()) {
+      throw new InvalidInputException(error.trim());
+    }
+
+    try {
+
+
+
+      for (Equipment equipment : climbSafe.getEquipment()) {
+        if (equipment.getName().equals(oldName)) {
+
+          equipment.setName(newName);
+          equipment.setWeight(newWeight);
+          equipment.setPricePerWeek(newPricePerWeek);
+
+        }
+
+      }
+
+
+    } catch (RuntimeException e) {
+      throw new InvalidInputException("Doesn't work");
+
+    }
+
+
+
+  }
+
+
 
 }
