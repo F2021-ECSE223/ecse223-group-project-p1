7a9f650ff4a445c91c78aea956715e7cba5a5111
Alexandre Chiasera <70693638+Chiasera@users.noreply.github.com>
2021-10-21 14:52:25 -0400

    Update P1StepDefinitions.java

--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P1StepDefinitions.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P1StepDefinitions.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.function.Executable;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
@@ -21,18 +22,15 @@ import ca.mcgill.ecse.climbsafe.model.Equipment;
 import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.an.E;
 
 public class P1StepDefinitions {
 
   private ClimbSafe climbSafe;
   private String error;
   private int errorCount;
-  boolean oldIsNewName = false;
 
   @Before
   public static void setUp() {
-
     // clear all data
     ClimbSafeApplication.getClimbSafe().delete();
   }
@@ -42,17 +40,13 @@ public class P1StepDefinitions {
    */
   @Given("the following ClimbSafe system exists: \\(p1)")
   public void the_following_climb_safe_system_exists_p1(io.cucumber.datatable.DataTable dataTable) {
-
     List<Object> columns = (dataTable.asLists(Object.class)).get(1);
 
     climbSafe = new ClimbSafe(Date.valueOf((String) columns.get(0)),
             Integer.parseInt((String) columns.get(1)), Integer.parseInt((String) columns.get(2)));
 
-
-
     error = "";
     errorCount = 0;
-
   }
 
   /**
@@ -66,10 +60,7 @@ public class P1StepDefinitions {
     for (var row : rows) {
       climbSafe.addEquipment(new Equipment(row.get("name"), Integer.parseInt(row.get("weight")),
               Integer.parseInt(row.get("pricePerWeek")), climbSafe));
-    }
-
-
-
+    } 
   }
 
   /**
@@ -108,140 +99,96 @@ public class P1StepDefinitions {
           var addedItemName = bufferItemList.get(x).getName();
           var itemQuantity = quantityList.get(x);
           if (existingEquipmentName.equals(addedItemName)) {
-            newBundle.addBundleItem(itemQuantity, climbSafe, climbSafe.getEquipment(i)); // add to
-            // empty
-            // Bundle
-            // pieces
-            // of
-            // equipments
-            // already
-            // existing
+            newBundle.addBundleItem(itemQuantity, climbSafe, climbSafe.getEquipment(i)); // add to empty Bundle previously created
           }
-
         }
       }
     }
   }
 
-
   /**
    * @author Asma Gandour
-   */
+   */ 
   @When("the administator attempts to update the piece of equipment in the system with name {string} to have name {string}, weight {string}, and price per week {string} \\(p1)")
   public void the_administator_attempts_to_update_the_piece_of_equipment_in_the_system_with_name_to_have_name_weight_and_price_per_week_p1(
           String string, String string2, String string3, String string4) {
-
-    if(string.equals(string2)) {
-      oldIsNewName = true;
-    }
-
     callController(() -> ClimbSafeFeatureSet4Controller.updateEquipment(string, string2,
             Integer.parseInt(string3), Integer.parseInt(string4)));
-
-
   }
 
   /**
    *
    * @author Haroun Guessous
-   */
+   */   
   @Then("the number of pieces of equipment in the system shall be {string} \\(p1)")
   public void the_number_of_pieces_of_equipment_in_the_system_shall_be_p1(String string) {
-
     assertEquals(Integer.parseInt(string), climbSafe.getEquipment().size());
-
   }
 
   /**
    *
    * @author Alexandre Chiasera
    */
-
   @Then("the piece of equipment with name {string}, weight {string}, and price per week {string} shall not exist in the system \\(p1)")
   public void the_piece_of_equipment_with_name_weight_and_price_per_week_shall_not_exist_in_the_system_p1(
           String string, String string2, String string3) {
-
-
     Integer weight = Integer.parseInt(string2);
     Integer pricePerWeek = Integer.parseInt(string3);
 
     for (Equipment pieceOfEquipment : climbSafe.getEquipment()) { // for each piece of equipment in
       // the climbSafe system (for the
       // admin)
-      if (pieceOfEquipment.getName().equals(string) && !oldIsNewName && pieceOfEquipment.getWeight() == weight
+      if (pieceOfEquipment.getName().equals(string) && pieceOfEquipment.getWeight() == weight
               && pieceOfEquipment.getPricePerWeek() == pricePerWeek) {
         fail("The piece of equipment <" + pieceOfEquipment.getName()
                 + "> has not been deleted from the system"); // if everything matches, it means the
-        // object is not deleted.
       }
     }
-    oldIsNewName = false;
   }
 
   /**
    *
    * @author Alexandre Chiasera
-   */
+   */ 
   @Then("the piece of equipment with name {string}, weight {string}, and price per week {string} shall exist in the system \\(p1)")
   public void the_piece_of_equipment_with_name_weight_and_price_per_week_shall_exist_in_the_system_p1(
           String string, String string2, String string3) {
-
     String name = string;
     Integer weight = Integer.parseInt(string2);
     Integer pricePerWeek = Integer.parseInt(string3);
-    Boolean isEquipmentInTheSystem = false;
-
-    for (Equipment pieceOfEquipment : climbSafe.getEquipment()) { // for each piece of equipment in
-      // the climbSafe system (for the
-      // admin)
-      if (pieceOfEquipment.getName().equals(name) && pieceOfEquipment.getWeight() == weight
-              && pieceOfEquipment.getPricePerWeek() == pricePerWeek) {
-        isEquipmentInTheSystem = true;// if everything matches --> object in the system.
-      }
-    }
-
-    assertEquals("", error);
-
-    assertEquals(true, isEquipmentInTheSystem);
-
-
-
+    var pieceOfEquipment = (Equipment)Equipment.getWithName(string);
+          
+      assertTrue(pieceOfEquipment.getName().equals(name) && pieceOfEquipment.getWeight() == weight
+    		    && pieceOfEquipment.getPricePerWeek() == pricePerWeek);
   }
 
+  
   /**
    * @author Atreyi Srivastava
    */
-
   @Then("the following pieces of equipment shall exist in the system: \\(p1)")
   public void the_following_pieces_of_equipment_shall_exist_in_the_system_p1(
           io.cucumber.datatable.DataTable dataTable) {
-
-    var table = dataTable.asMaps();
+    var rows = dataTable.asMaps();
     Boolean alreadyExist = false;
-    for (var val : table) {
-      String name = val.get("name");
-      int weight = Integer.parseInt(val.get("weight"));
-      int pricePerWeek = Integer.parseInt(val.get("pricePerWeek"));
+    for (var row : rows) {
+      	
+      String name = row.get("name");
+      int weight = Integer.parseInt(row.get("weight"));
+      int pricePerWeek = Integer.parseInt(row.get("pricePerWeek"));
+      var pieceOfEquipment = (Equipment) Equipment.getWithName(name);
 
-      for (Equipment equipments : climbSafe.getEquipment()) {
-        if (equipments.getName().equals(name) && equipments.getWeight() == weight
-                && equipments.getPricePerWeek() == pricePerWeek) {
-          alreadyExist = true;
-        }
-      }
-    }
-    assertEquals(true, alreadyExist);
+      assertTrue(pieceOfEquipment.getName().equals(name) && pieceOfEquipment.getWeight() == weight
+  		    && pieceOfEquipment.getPricePerWeek() == pricePerWeek);
+	    }
   }
 
   /**
    * @author Mohammad Shaheer Bilal
    */
-
   @Then("the system shall raise the error {string} \\(p1)")
   public void the_system_shall_raise_the_error_p1(String string) {
-
     assertEquals(string, error);
-
   }
 
   /**
@@ -250,33 +197,22 @@ public class P1StepDefinitions {
    */
   @Then("the number of equipment bundles in the system shall be {string} \\(p1)")
   public void the_number_of_equipment_bundles_in_the_system_shall_be_p1(String string) {
-
     assertEquals(Integer.parseInt(string), climbSafe.getBundles().size());
-
   }
 
   /**
    * @author Atreyi Srivastava
    */
-
   @Then("the following equipment bundles shall exist in the system: \\(p1)")
   public void the_following_equipment_bundles_shall_exist_in_the_system_p1(
           io.cucumber.datatable.DataTable dataTable) {
-
-    var table = dataTable.asMaps();
-    Boolean alreadyExist = false;
-    for (var val : table) {
-      String nameBundle = val.get("name");
-      int discount = Integer.parseInt(val.get("discount"));
-
-      for (EquipmentBundle bundle : climbSafe.getBundles()) {
-        if (bundle.getName().equals(nameBundle) && bundle.getDiscount() == discount) {
-          alreadyExist = true;
-        }
-      }
+    var rows = dataTable.asMaps();
+    for (var row : rows) {
+      String bundleName = row.get("name");
+      int discount = Integer.parseInt(row.get("discount"));
+      var equipmentBundle = (EquipmentBundle)EquipmentBundle.getWithName(bundleName);
+      assertTrue(equipmentBundle.getName().equals(bundleName) && equipmentBundle.getDiscount() == discount);
     }
-    assertEquals(true, alreadyExist);
-
   }
 
   @After
@@ -294,5 +230,4 @@ public class P1StepDefinitions {
       fail();
     }
   }
-
 }
