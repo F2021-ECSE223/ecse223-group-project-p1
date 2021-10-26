011640ffa0fadebaee82b3d57b3a7f555b9b1ec4
aasmaa11 <90011767+aasmaa11@users.noreply.github.com>
2021-10-26 00:22:39 -0400

    Update ClimbSafeFeatureSet4Controller.java
    Added JavaDoc and re-formatted

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -1,5 +1,4 @@
 package ca.mcgill.ecse.climbsafe.controller;
-
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 import ca.mcgill.ecse.climbsafe.model.BookableItem;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
@@ -13,12 +12,23 @@ public class ClimbSafeFeatureSet4Controller {
   public static void addEquipment(String name, int weight, int pricePerWeek)
       throws InvalidInputException {}
 
+  /**
+   * This method update an equipment with name oldName with
+   * a new name, weight and price per week. It starts by checking
+   * if the inputs are valid.
+   * 
+   * @author Asma Gandour
+   * @param oldName
+   * @param newName
+   * @param newWeight
+   * @param newPricePerWeek
+   * @throws InvalidInputException
+   */
   public static void updateEquipment(String oldName, String newName, int newWeight,
       int newPricePerWeek) throws InvalidInputException {
-
     var error = "";
     Equipment foundEquipment = null;
-    
+
     if (newWeight <= 0) {
       error += "The weight must be greater than 0";
     }
@@ -28,45 +38,35 @@ public class ClimbSafeFeatureSet4Controller {
     if (newName.isEmpty()) {
       error += "The name must not be empty";
     }
-    
-    if( !newName.equals(oldName) && BookableItem.hasWithName(newName)) {
-      
-      if(BookableItem.getWithName(newName) instanceof EquipmentBundle) {
+
+    if (!newName.equals(oldName) && BookableItem.hasWithName(newName)) {
+
+      if (BookableItem.getWithName(newName) instanceof EquipmentBundle) {
         error = "An equipment bundle with the same name already exists";
-      }
-      else {
+      } else {
         error = "The piece of equipment already exists";
       }
     }
 
     foundEquipment = (Equipment) Equipment.getWithName(oldName);
 
-    
-    
-    if(foundEquipment == null) {
+    if (foundEquipment == null) {
       error = "The piece of equipment does not exist";
     }
-    
-    
+
     if (!error.isEmpty()) {
       throw new InvalidInputException(error.trim());
     }
-      
+
     try {
 
       foundEquipment.setName(newName);
       foundEquipment.setWeight(newWeight);
       foundEquipment.setPricePerWeek(newPricePerWeek);
-     
+
     } catch (RuntimeException e) {
-      
+
       throw new InvalidInputException(e.getMessage());
     }
-
-
-
   }
-
-
-
 }
