77e1f62069617ecd33ba198fef7a1251fc106005
mohammadShaheerb <90063886+mohammadShaheerb@users.noreply.github.com>
2021-10-28 00:24:48 -0400

    Update ClimbSafeFeatureSet4Controller.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -9,8 +9,46 @@ public class ClimbSafeFeatureSet4Controller {
 
   public static ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
 
+    
+  /**
+   * This method update an equipment with name oldName with
+   * a new name, weight and price per week. It starts by checking
+   * if the inputs are valid.
+   * 
+   * @author Mohammad Shaheer Bilal 
+   * @param name
+   * @param weight
+   * @param priceperWeek
+   * @throws InvalidInputException
+   */
+
   public static void addEquipment(String name, int weight, int pricePerWeek)
-      throws InvalidInputException {}
+      throws InvalidInputException {
+	    
+
+	    if (name.isEmpty()) {
+	    	throw new InvalidInputException("The name must not be empty") ;
+	    }
+	    if (pricePerWeek < 0) {
+	    	throw new InvalidInputException("The price per week must be greater than or equal to 0");
+	    }
+	    if (weight<=0) {
+	    	throw new InvalidInputException("The weight must be greater than 0");
+	    }
+	    
+	    if (BookableItem.hasWithName(name)) {
+	    	if (BookableItem.getWithName(name) instanceof EquipmentBundle) {
+	    		throw new InvalidInputException("The equipment bundle already exists")  ;
+	          } 
+	    	else {
+	        	  throw new InvalidInputException("The piece of equipment already exists");
+	          }
+	    } 
+	    
+	    Equipment newEquipment = new Equipment(name, weight, pricePerWeek, climbSafe);
+
+	  
+  }
 
   /**
    * This method update an equipment with name oldName with
