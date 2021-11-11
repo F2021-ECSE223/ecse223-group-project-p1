9359986411282dff56a93d19fa49efcdb7dbdec1
AaronQLF <88989480+AaronQLF@users.noreply.github.com>
2021-11-11 14:10:17 -0500

    Update AssignmentController.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -45,21 +45,20 @@ public class AssignmentController {
      * @param member
      * @throws InvalidInputException 
      */
-  public static void payment(String email, String paymentAuthorizationCode) throws InvalidInputException{
-	Member member= (Member)User.getWithEmail(email);
+public static void payment(String email, String paymentAuthorizationCode) throws InvalidInputException{
+	  Member member= (Member)Member.getWithEmail(email);
       if(member==null){
        throw new InvalidInputException("Member with email address"+email+"does not exist"); 
       }
-    	if (member.getMemberStatusFullName().equals("Banned"))
-    			{
-    		throw new InvalidInputException("Cannot pay for a banned member.");
-    			}
-    	else {
-    	if (member.getMemberStatusFullName().equals("Finished"))
-    	{
-    		throw new InvalidInputException("Cannot pay for a finished trip");
-    	}	
-    	else {
+      switch(member.getMemberStatusFullName())
+      {
+      case "Cancelled":
+    	  throw new InvalidInputException("Cannot pay for a trip which has been cancelled");
+      case "Finished":
+    	  throw new InvalidInputException("Cannot pay for a trip which has finished");
+      case "Banned":
+    	  throw new InvalidInputException("Cannot cancel the trip due to a ban");
+      }
     	var assignment= member.getAssignment();
     	if (assignment.getAssignmentStatusFullName().equals("Paid") || assignment.getAssignmentStatusFullName().equals("Started")) {throw new InvalidInputException("Member has already paid for their trip.");}
     	else {
@@ -69,11 +68,9 @@ public class AssignmentController {
             ClimbSafePersistence.save();
           } catch (RuntimeException e) {
             throw new InvalidInputException(e.getMessage());
-          }
+          } 
         }
     	}
-    	}
-    }
  /**
      * This method starts all the trips for a given week. If the member is banned (due to no payment), the trip cannot be started. 
      * @author Alexandre Chiasera
